283ff2a1e075fd13db810287e49e30fe870d767c
sedef <sedef.bingul@gmail.com>
2021-06-02 11:50:24 -0400

    [sedef] created separate tasks for sql, selenium, postmant

--- auto_test/biletix.py
+++ auto_test/biletix.py
@@ -45,57 +45,22 @@ webwait.until(
 
 print(" ************ Part B *******************")
 
-rowLoc = "//div[@id='paginator']/ul/li"
 
-allrowsele = webwait.until(EC.presence_of_all_elements_located((By.XPATH, rowLoc)))
 
-allheadersnames = ["EventName", "EventStatus", "EventDate"]
-allCollecitonList = [OrderedDict()]
-clickCount = 3  # clicking 3 times because we have 4 pages
+#ı expected
+# to creat list
+#events = [eventName, eventStatus, eventDateOne + eventDateTwo] for each row by for loop with using incrementing (eventCount =+1 )
+#and put the result inside dictionary(collectionList) by allCollectionList.append(eachrow)
 
-for clickCount in range(clickCount, len(allrowsele)):
-    eventRowLoc = "//*[@id='all_result']/div[4]"
-    specificRowLoc = f"//div[@id='paginator']/ul/li[{clickCount}]"  # 2.sayfanın alanı
+allCollecitonList = []
+events = [eventName, eventStatus, eventDateOne + eventDateTwo]
 
-    allResultLoc = driver.find_element_by_xpath(eventRowLoc)
-    allResultEventList = allResultLoc.find_elements_by_class_name("grid_21")
-    # row_xpath = "//div[@class='result_render']//div[@class='grid_21 alpha omega listevent searchResultEvent']"
-
-    eventCount = 1
-    for allResult in allResultEventList:
-        eventNameLoc = f"//*[@id=\"all_result\"]/div[4]/div[{eventCount}]/div/div[3]/div[2]/a"
-        eventStatusLoc = f"//*[@id=\"all_result\"]/div[4]/div[{eventCount}]/div/div[3]/div[2]/span"
-        eventDateOneLoc = f"//*[@id=\"all_result\"]/div[4]/div[{eventCount}]/div/div[5]/div/span[1]"
-        eventDateTwoLoc = f"//*[@id=\"all_result\"]/div[4]/div[{eventCount}]/div/div[5]/div/span[2]"
-
-        # reading the each event row
-        eventName = allResult.find_element_by_xpath(eventNameLoc).text
-        eventStatus = allResult.find_element_by_xpath(eventStatusLoc).text
-        eventDateOne = allResult.find_element_by_xpath(eventDateOneLoc).text
-        eventDateTwo = allResult.find_element_by_xpath(eventDateTwoLoc).text
-
-        # saving it in the list
-        events = [eventName, eventStatus, eventDateOne + eventDateTwo]
-
-        # saving the values from 'events' list in the dictionary called eachRowData
-        eachRowData = OrderedDict()
-        for j in range(len(allheadersnames)):
-            eachRowData[allheadersnames[j]] = events[j]
-
-        # incrementing to go to next row
-        eventCount = eventCount + 1
-
-        print("# adding the dictionary of each row to the main list of events")
-        allCollecitonList.append(eachRowData)
-
-    print("# clicks next page after getting all rows from the current page")
-    driver.find_element_by_xpath(specificRowLoc).click()
 
 print("# printing the big list of all events")
 print(allCollecitonList)
 
 print(" ************ Part C *******************")
-collection = OrderedDict()
+
 
 print("# 1. removing the record with date 'Paz, 10/01/21' from the result set")
 for collection in allCollecitonList:
